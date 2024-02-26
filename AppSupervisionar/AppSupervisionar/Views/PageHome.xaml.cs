b519f0b1b086caf1de31fa246252d83a160acc62
Victor <joaossilvapereira@gmail.com>
2024-02-26 16:34:34 -0300

    Adicionado P.Cadastrar, P.Detalhes e P.Home

--- AppSupervisionar/AppSupervisionar/Views/PageHome.xaml.cs
+++ AppSupervisionar/AppSupervisionar/Views/PageHome.xaml.cs
@@ -10,49 +10,34 @@ using Xamarin.Forms.Xaml;
 namespace AppSupervisionar.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class PagePrincipal : MasterDetailPage
+    public partial class PageHome : ContentPage
     {
-        public PagePrincipal()
+        public PageHome()
         {
             InitializeComponent();
-            btHome_Clicked(new object(), new EventArgs());
             Task.Run(AnimatedBackground);
         }
-       
         private async void AnimatedBackground()
         {
             Action<double> forward = input => bcGradient.AnchorY = input;
             Action<double> backward = input => bcGradient.AnchorY = input;
             while (true)
             {
-                bcGradient.Animate(name: "forward", callback: forward, start: 0, end: 1, length:5000,easing: Easing.SinIn);
+                bcGradient.Animate(name: "forward", callback: forward, start: 0, end: 1, length: 5000, easing: Easing.SinIn);
                 await Task.Delay(5000);
                 bcGradient.Animate(name: "backward", callback: backward, start: 1, end: 0, length: 5000, easing: Easing.SinIn);
                 await Task.Delay(5000);
             }
-           
         }
 
-        private void btHome_Clicked(object sender, EventArgs e)
-        {
-            Detail = new NavigationPage(new PageHome());
-            IsPresented = false;
-        }
-
-        private void btCadastrar_Clicked(object sender, EventArgs e)
+        private void qtdCadastrar_Tapped(object sender, EventArgs e)
         {
 
         }
 
-        private void btLocalizar_Clicked(object sender, EventArgs e)
+        private void qtdLocalizar_Tapped(object sender, EventArgs e)
         {
 
-        }
-
-        private void btDetalhes_Clicked(object sender, EventArgs e)
-        {
-            Detail = new NavigationPage(new PageDetalhes());
-            IsPresented = false;
         }
     }
 }
